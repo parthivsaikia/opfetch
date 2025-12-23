83908bb827fa13070b8285565c10901d597394d1
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-12-24 00:38:42 +0530

    feat(package): add implementation to fetch package count in arch

---
+++ internal/fetcher/package.go
@@ -0,0 +1,29 @@
+package fetcher
+
+import (
+	"os"
+)
+
+func GetPackageCount(filepath string) (int, error) {
+	pacmanFilePath := "/var/lib/pacman/local"
+
+}
+
+func getPacmanPackage() (int, error) {
+	count := 0
+	entries, err := os.ReadDir("/var/lib/pacman/local")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return 0, nil
+		}
+		return 0, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			count++
+		}
+	}
+	return count, nil
+
+}
